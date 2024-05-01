ba582cb9fdce1c7f56ab97ba9bd09adb2c4d3066
Casper07777 <100747003+Casper07777@users.noreply.github.com>
2024-05-01 17:27:57 +0530

    change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import GenerateImageRouter from "./routes/GenerateImage.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: true,credentials: true}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
